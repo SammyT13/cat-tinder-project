e0f899f967ae80e6a47b7bcdddeda540f5311041
SammyT13 <sammyt1316@yahoo.com>
2023-02-03 19:46:30 -0800

    completed api-validations

--- spec/models/cat_spec.rb
+++ spec/models/cat_spec.rb
@@ -1,5 +1,44 @@
 require 'rails_helper'
 
 RSpec.describe Cat, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "should be valid that cat cannot be created without a name" do
+    # We removed the name, since we're testing for the name to be in the model
+    cat = Cat.create(
+      age: 9,
+      enjoys: 'looking out the window',
+      image: 'https://media.istockphoto.com/id/1361956153/photo/black-cat-sticking-out-tongue-funny-portrait.jpg?b=1&s=170667a&w=0&k=20&c=O6mSvSQiiXT-8Vgfc71ScUQ8manUnq-dtcKTwSCq_bY='
+    )
+    expect(cat.errors[:name]).to_not be_empty
+  end
+  
+  it "should be valid that cat cannot be created without an age" do
+    # We removed the name, since we're testing for the name to be in the model
+    cat = Cat.create(
+      name: 'Kevin',
+      enjoys: 'looking out the window',
+      image: 'https://media.istockphoto.com/id/1361956153/photo/black-cat-sticking-out-tongue-funny-portrait.jpg?b=1&s=170667a&w=0&k=20&c=O6mSvSQiiXT-8Vgfc71ScUQ8manUnq-dtcKTwSCq_bY='
+    )
+    expect(cat.errors[:age]).to_not be_empty
+  end 
+
+  it "should be valid that cat cannot be created without an enjoys" do
+    # We removed the name, since we're testing for the name to be in the model
+    cat = Cat.create(
+      name: 'Kevin',
+      age: 9,
+      image: 'https://media.istockphoto.com/id/1361956153/photo/black-cat-sticking-out-tongue-funny-portrait.jpg?b=1&s=170667a&w=0&k=20&c=O6mSvSQiiXT-8Vgfc71ScUQ8manUnq-dtcKTwSCq_bY='
+    )
+    expect(cat.errors[:enjoys]).to_not be_empty
+  end 
+
+  it "should be validate that an enjoy entry has a minimum of 10 characters" do
+    # We removed the name, since we're testing for the name to be in the model
+    cat = Cat.create(
+      name: 'Kevin',
+      age: 9,
+      enjoys: 'looking',
+      image: 'https://media.istockphoto.com/id/1361956153/photo/black-cat-sticking-out-tongue-funny-portrait.jpg?b=1&s=170667a&w=0&k=20&c=O6mSvSQiiXT-8Vgfc71ScUQ8manUnq-dtcKTwSCq_bY='
+    )
+    expect(cat.errors[:enjoys]).to_not be_empty
+  end 
 end
